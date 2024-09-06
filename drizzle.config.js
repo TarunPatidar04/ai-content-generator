31b1c4fcc01734ad73795978177590b68fd1b0bc
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-09-06 14:17:46 +0530

    setup postgresql database (neon postgresql)

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.tsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: "postgresql://Ai-Content-Generator_owner:8cGtkMBQzp1h@ep-jolly-rain-a5meandh.us-east-2.aws.neon.tech/Ai-Content-Generator?sslmode=require",
+    }
+};
